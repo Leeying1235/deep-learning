77e0dc717d7973a96f600979efca805c01714c23
Leeying1235 <34576919+Leeying1235@users.noreply.github.com>
2019-07-19 11:25:46 +0800

    Update flatten.py

--- src/flatten.py
+++ src/flatten.py
@@ -1,3 +1,4 @@
+'''
 from layer import Layer
 class Flatten(Layer):
 	def __init__(self):
@@ -9,3 +10,18 @@ class Flatten(Layer):
 		return self.__output
 	def back(self,error,leraning_rate):
 		return self.__output.reshape(self.__shape) 
+'''
+import numpy as np
+from layer import Layer
+class Flatten(Layer):
+	def __init__(self):
+		super(Flatten,self).__init__()
+	def forward_propagation(self,_input):
+		self.__input=_input
+		self.__shape=self.__input.shape
+		self.__output=np.zeros((self.__shape[0],reduce(lambda x,y:x*y,self.__shape[1:])))
+		for i in range(self.__shape[0]):
+			self.__output[i,:]=self.__input[i,:,:,:].flatten()
+		return self.__output
+	def back_propagation(self,error,leraning_rate):
+		return self.__output.flatten().reshape(self.__shape) 
